e0b418f39e69c4d78ece0c8c60ddb6edc154a51b
kalpanibhagya <kalpanibhagya.kb@gmail.com>
2018-10-01 12:01:07 +0530

    Add files

---
+++ Controllers/RecordsController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using NewWorld.Models;
+
+namespace NewWorld.Controllers
+{
+    public class RecordsController : Controller
+    {
+        // GET: Records
+        public ActionResult Index()
+        {
+            Record rec = new Record
+            {
+                Id = 101,
+                RecordName = "Bouchers",
+                RecordDetails = "The basic stocks"
+            };
+            ViewBag.Message = rec;
+            return View();
+        }
+    }
+}
---
+++ Models/Record.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NewWorld.Models
+{
+    public class Record
+    {
+        public int Id { get; set; }
+        public string RecordName { get; set; }
+        public string RecordDetails { get; set; }
+    }
+}
